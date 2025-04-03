5288177a6e9f0d066e80ec9a02066161e10f41dd
AkshatJha21 <akshatj2134@gmail.com>
2025-04-03 14:33:05 +0530

    01 oops

---
+++ day4/oops.js
@@ -0,0 +1,14 @@
+console.log("oops");
+
+// procedural programming
+let name = "John";
+let age = 25;
+
+const printUser = (name, age) => {
+    console.log(`Name: ${name}, Age: ${age}`);
+};
+
+printUser(name, age);
+
+// object oriented programming paradigm
+
