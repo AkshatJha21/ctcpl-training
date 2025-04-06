8403a94a6eeebecf4d405ff834b7b24103f8172f
Akshat <akshat.jha@tcnbroadcasting.com>
2025-04-06 17:57:44 +0530

    procedural vs oops

--- day4/oops.js
+++ day4/oops.js
@@ -1,14 +1,30 @@
 console.log("oops");
 
-// procedural programming
+// procedural programming: code is structured as a sequence of instructions 
 let name = "John";
-let age = 24;
+let age = 25;
 
 const printUser = (name, age) => {
-    console.log(`Name:- ${name}, Age:- ${age}`);
+    console.log(`Name: ${name}, Age: ${age}`);
 };
 
 printUser(name, age);
 
-// object oriented programming paradigm
+// object oriented programming: data and functions are combined into objects
 
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    printUser() {
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+}
+
+const user1 = new User("Akshat", 21);
+const user2 = new User("Jack", 22);
+
+user1.printUser();
+user2.printUser();
