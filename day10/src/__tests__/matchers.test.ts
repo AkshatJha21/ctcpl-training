381685ca98efbf67121c99a8083ce0e7ae41ff3f
Akshat <akshat.jha@tcnbroadcasting.com>
2025-04-15 16:52:20 +0530

    rmf

--- day10/src/__tests__/matchers.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@jest/globals";
-
-test('basic matchers', () => {
-    expect(1 + 1).toBe(2);
-    expect('Akshat').toBeDefined();
-    expect([1, 2, 3]).toContain(2);
-})
